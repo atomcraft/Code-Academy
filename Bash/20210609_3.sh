a956e40c8ddb6778cfba1d9c0d111352800c7a07
Delyan Gavrailov <43825159+atomcraft@users.noreply.github.com>
2021-06-11 10:10:10 +0300

    Update 20210609_3.sh

--- Bash/20210609_3.sh
+++ Bash/20210609_3.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-TEAM=$(((RANDOM%32)+1))
-echo $TEAM
+
+member=0
+MAX_MEMBERS_COUNT=3
+member_count=1
+teams_count=11
+count_teams=1
+TEAM_MAX_COUNT=11
+while [ "$count_teams" -le $TEAM_MAX_COUNT ]
+do
+	printf "Team $count_teams\n"
+	while [ "$member_count" -le $MAX_MEMBERS_COUNT ]
+	do
+		member=$(((RANDOM%32)+1))
+		echo "$member"
+		let "member_count += 1"  
+	done
+	let "count_teams +=1"
+done
